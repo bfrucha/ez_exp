a4b0b6b0c35bc84ed3b75d2c093f9c6eb95a4622
Bruno FRUCHARD <bruno.fruchard@telecom-paristech.fr>
2018-01-05 10:13:46 +0100

    fix(path): problems with directory paths

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-eval(require('fs').readFileSync('./lib/experiment.js', 'utf8'));
+eval(require('fs').readFileSync(__dirname+'/lib/experiment.js', 'utf8'));
 
 // create a new experiment object
 // parameters given are the path to the parameter file, the name of the current participant and the name of the column where to find participant ids
-var experiment = Experiment.loadExperiment('example_data/exp_parameters.csv', 'user-1', 'Participant');
+var experiment = Experiment.loadExperiment(__dirname+'/example_data/exp_parameters.csv', 'user-1', 'Participant');
 
 // default path to record data is 'results_[participant ID].csv'
-experiment.setOutputPath('example_data/results_user-1.csv');
+experiment.setOutputPath(__dirname+'/example_data/results_user-1.csv');
 
 // load information
 experiment.loadNextTrial();
--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 var fs = require('fs');
-var ez_exp = eval(fs.readFileSync('lib/experiment.js', 'utf8'));
-module.exports.enums = eval(fs.readFileSync('lib/enum.js', 'utf8'));
-
+var ez_exp = eval(fs.readFileSync(__dirname+'/lib/experiment.js', 'utf8'));
+// loading enums used in the module
+eval(fs.readFileSync(__dirname+'/lib/enum.js', 'utf8'));
 module.exports.loadExperiment = Experiment.loadExperiment;
-
-module.exports.test = function () {
-  var t1 = exp.createTrial(['1','2']);
-  var t2 = exp.createTrial(['3','4']);
-
-  t1.startTrial();
-  var i = 0;
-  while(i++ < 1000000);
-  t2.startTrial();
-  console.log(t1.getMainDuration(), t2.getMainDuration());
-};
