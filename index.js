72df47db6d487726c7daeca2482ead5de080f4c2
Bruno FRUCHARD <bruno.fruchard@telecom-paristech.fr>
2018-01-17 19:19:45 +0100

    fix(minors): mostly syntax problems

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var fs = require('fs');
 var ez_exp = eval(fs.readFileSync(__dirname+'/lib/experiment.js', 'utf8'));
 // loading enums used in the module
 eval(fs.readFileSync(__dirname+'/lib/enum.js', 'utf8'));
+module.exports.Enums = EzExp.Enums;
 module.exports.loadExperiment = EzExp.loadExperiment;
